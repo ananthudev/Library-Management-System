08c22a79070704215ab3cc6435a59aec246bb5f9
Ananthu Dev <79274178+ananthudev@users.noreply.github.com>
2023-02-12 04:00:33 +0530

    Added Dropdown menu to fetch data from database without HTMl Features added

--- UpdateBooks.php
+++ UpdateBooks.php
@@ -1,67 +1,73 @@
-<!DOCTYPE HTML>
-<html>
-<head>
-    <title>Library Management System</title>
-</head>
-<body bgcolor="87ceeb">
-    <center>
-        <h2>Library Management System</h2>
-    </center>
-    <br>
-    <!--Once the form is submitted, all the form data is forwarded to UpdateBooks2.php -->
-    <form action="UpdateBooks2.php" method="post">
-        <table border="2" align="center" cellpadding="5" cellspacing="5">
-            <tr>
-            <td>Enter ISBN :</td>
-            <td>
-                <select name="isbn">
-                    <option value="">--Select ISBN--</option>
-                    <?php
-                        //connect to database
-                        $conn = mysqli_connect("localhost", "root", "", "books");
-                        //check the connection
-                        if (!$conn) {
-                            die("Connection failed: " . mysqli_connect_error());
-                        }
-                        //select all ISBNs from the books table
-                        $sql = "SELECT ISBN FROM books";
-                        $result = mysqli_query($conn, $sql);
-                        if (mysqli_num_rows($result) > 0) {
-                            //output data for each row
-                            while($row = mysqli_fetch_assoc($result)) {
-                                echo "<option value='".$row["ISBN"]."'>".$row["ISBN"]."</option>";
-                            }
-                        }
-                        //close the database connection
-                        mysqli_close($conn);
-                    ?>
-                </select>
-            </td>
-            </tr>
-            <tr>
-            <td>Enter Title :</td>
-            <td><input type="text" name="title" size="48"></td>
-            </tr>
-            <tr>
-            <td>Enter Author :</td>
-            <td><input type="text" name="author" size="48"></td>
-            </tr>
-            <tr>
-            <td>Enter Edition :</td>
-            <td><input type="text" name="edition" size="48"></td>
-            </tr>
-            <tr>
-            <td>Enter Publication :</td>
-            <td><input type="text" name="publication" size="48"></td>
-            </tr>
-            <tr>
-            <td></td>
-            <td>
-                <input type="submit" value="Submit">
-                <input type="reset" value="Reset">
-            </td>
-            </tr>
-        </table>
-    </form>
+<?php
+include("DBConnection.php");
+
+$ISBN = isset($_REQUEST["ISBN"]) ? $_REQUEST["ISBN"] : '';
+$Title = isset($_REQUEST["Title"]) ? $_REQUEST["Title"] : '';
+$Author = isset($_REQUEST["Author"]) ? $_REQUEST["Author"] : '';
+$Edition = isset($_REQUEST["Edition"]) ? $_REQUEST["Edition"] : '';
+$Publication = isset($_REQUEST["Publication"]) ? $_REQUEST["Publication"] : '';
+
+if(isset($_REQUEST["update"]))
+{
+    $query = "update book_info set Title='$Title',Author='$Author',Edition='$Edition',Publication='$Publication' where ISBN='$ISBN'";
+    $result = mysqli_query($db,$query);
+    if($result)
+    {
+        echo "<center>Record updated successfully</center>";
+    }
+    else
+    {
+        echo "<center>Record not updated</center>";
+    }
+}
+
+$query = "select ISBN from book_info";
+$result = mysqli_query($db,$query);
+?>
+
+<form action="#" method="post">
+<table align="center">
+    <tr>
+        <td>Select ISBN</td>
+        <td>
+            <select name="ISBN">
+            <option value="">Select ISBN</option>
+            <?php while($row = mysqli_fetch_assoc($result))
+            {
+                echo "<option value='".$row['ISBN']."'>".$row['ISBN']."</option>";
+            }
+            ?>
+            </select>
+        </td>
+    </tr>
+    <tr>
+        <td>Title</td>
+        <td><input type="text" name="Title" value=""></td>
+    </tr>
+    <tr>
+        <td>Author</td>
+        <td><input type="text" name="Author" value=""></td>
+    </tr>
+    <tr>
+        <td>Edition</td>
+        <td><input type="text" name="Edition" value=""></td>
+    </tr>
+    <tr>
+        <td>Publication</td>
+        <td><input type="text" name="Publication" value=""></td>
+    </tr>
+    <tr>
+        <td><input type="submit" name="update" value="Update"></td>
+    </tr>
+</table>
+</form>
+
+<br>
+<a href="SearchBooks.php"> To Search Book Information,Click here </a>
+<br>
+<a href="DisplayBooks.php"> To Display Book Information,Click here </a>
+<br>
+<a href="EnterBooks.php"> To Enter Book information,,Click here </a>
+
 </body>
 </html>

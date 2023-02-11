6057e7a6eb1ffbb88f6f68ec23f39d0d46ab78b0
Ananthu Dev <79274178+ananthudev@users.noreply.github.com>
2023-02-11 22:27:51 +0530

    Added Code

--- UpdateBooks1.php
+++ UpdateBooks1.php
@@ -9,25 +9,28 @@
     </center>
     <br>
     <?php
-        include("DBConnection.php");
-        if (isset($_POST["isbn"]) && isset($_POST["title"]) && isset($_POST["author"]) && isset($_POST["edition"]) && isset($_POST["publication"])) {
-            $isbn = $_POST["isbn"];
-            $title = $_POST["title"];
-            $author = $_POST["author"];
-            $edition = $_POST["edition"];
-            $publication = $_POST["publication"];
-            $query = "SELECT * FROM book_info WHERE isbn = '$isbn'";
-            $result = mysqli_query($db, $query);
-            if (mysqli_num_rows($result) > 0) {
-                echo "<script type='text/javascript'>alert('Error: ISBN already exists. Book already exists.'); window.location.href='EnterBooks.php';</script>";
-            } else {
-                $query = "insert into book_info(isbn,title,author,edition,publication) values('$isbn','$title','$author','$edition','$publication')"; //Insert query to add book details into the book_info table
-                $result = mysqli_query($db, $query);
-                echo "<script type='text/javascript'>alert('Book information is inserted successfully'); window.location.href='EnterBooks.php';</script>";
-            }
-    
+    include("DBConnection.php");
+    if (isset($_POST["isbn"]) && isset($_POST["title"]) && isset($_POST["author"]) && isset($_POST["edition"]) && isset($_POST["publication"])) {
+        $isbn = $_POST["isbn"];
+        $title = $_POST["title"];
+        $author = $_POST["author"];
+        $edition = $_POST["edition"];
+        $publication = $_POST["publication"];
+        $query = "SELECT * FROM book_info WHERE isbn = '$isbn'";
+        $result = mysqli_query($db, $query);
+        if (mysqli_num_rows($result) > 0) {
+            echo "<script type='text/javascript'>alert('Error: ISBN already exists. Book already exists.'); window.location.href='EnterBooks.php';</script>";
         } else {
-            echo "<script type='text/javascript'>alert('Error: Missing information to insert book.'); window.location.href='EnterBooks.php';</script>";
+            $query = "insert into book_info(isbn,title,author,edition,publication) values('$isbn','$title','$author','$edition','$publication')"; //Insert query to add book details into the book_info table
+            $result = mysqli_query($db, $query);
+            echo "<script type='text/javascript'>alert('Book information is inserted successfully'); window.location.href='EnterBooks.php';</script>";
+        }
+
+    } else {
+        echo "<script type='text/javascript'>alert('Error: Missing information to insert book.'); window.location.href='EnterBooks.php';</script>";
+    }
+?>
+
         }
     ?>
 </body>

5f415cb19df5b49f0aecad21ff6aa2216f349d67
ananthudev <ananthudevdv@gmail.com>
2023-02-12 16:19:41 +0530

    Final Project with All features added

--- DBConnection.php
+++ DBConnection.php
@@ -7,8 +7,3 @@ define('DB_DATABASE', 'books'); //where books is the database name
 $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
 
-<!--Pinne phpmyadminil poyi, oru database create cheyanam books enna data base, athinte sql query njn ithine koode attach cheythit unde -->
-<!-- allenkil sql file tharam import cheythal mathi -->
-<!-- enthenkilum doubt undenkil anjana missinodo ennodo, chodichal mathi -->
-
-

9485cc153103af72a24c33240c91eb4de7694033
ananthudev <ananthudevdv@gmail.com>
2023-02-12 13:30:01 +0530

    Added Changes

--- DBConnection.php
+++ DBConnection.php
@@ -10,3 +10,5 @@ $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 <!--Pinne phpmyadminil poyi, oru database create cheyanam books enna data base, athinte sql query njn ithine koode attach cheythit unde -->
 <!-- allenkil sql file tharam import cheythal mathi -->
 <!-- enthenkilum doubt undenkil anjana missinodo ennodo, chodichal mathi -->
+
+

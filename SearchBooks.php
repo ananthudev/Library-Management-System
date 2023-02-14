46acb3bf0c5fb5e5f23a30bdedff30d216fff51c
ananthudev <ananthudevdv@gmail.com>
2023-02-14 20:29:57 +0530

    Corrected Hyperlinks in SearchBooks.php

--- SearchBooks.php
+++ SearchBooks.php
@@ -11,13 +11,13 @@
         </center>
         <br>
         </form>
-        <a href="EnterBooks.php"> To Enter Book information Click here </a>
+        <a href="EnterBooks.php"> To Enter Books information Click here </a>
         <br>
-        <a href="DisplayBooks.php"> To Display Book Information,Click here </a>
+        <a href="DisplayBooks.php"> To Display or Delete Books Information,Click here </a>
         <br>
-        <a href="SearchBooks.php"> To Search Book Information,Click here </a>
+        <a href="SearchBooks.php"> To Search Books Information,Click here </a>
         <br>
-        <a href="UpdateBooks.php"> To Update Book Information,Click here </a>
+        <a href="UpdateBooks.php"> To Update Books Information,Click here </a>
         
 
     </body>
